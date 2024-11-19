ef3d5a8118a0966bf42f479e5159a928368b2891
Hassanalmaymn <144214579+Hassanalmaymn@users.noreply.github.com>
2024-11-19 12:32:22 +0300

    adding some methods

--- account.c
+++ account.c
@@ -34,11 +34,52 @@ Account enter_Account_information()
     getchar();
 
     printf("Enter account holder name:");
-    fgets(a.holderName, 24, stdin);
+    fgets(a.holderName, 49, stdin);
 
     printf("Enter account holder email:");
-    fgets(a.holderEmail, 24, stdin);
+    fgets(a.holderEmail, 69, stdin);
     a.balance = 0;
 
     return a;
+}
+void print_student_data(Account* a)
+{
+    printf("\n-----------------\n");
+    printf("account's id: %ld\n", a->id);
+    printf("account's holder name: %s", a->holderName);
+    printf("account's holder email: %s", a->holderEmail);
+    printf("account's balance: %lf", a->balance);
+    printf("number of operations: %d\n", a->id);
+    printf("-----------------\n");
+}
+Account* get_students_data(char* fn, int* num)
+{
+    FILE* fp;
+    Account* result;
+    int i;
+
+    if ((fp = fopen(fn, "r")))
+    {
+        fread(num, sizeof(int), 1, fp);
+        result = (Account*)calloc(*num, sizeof(Account));
+
+        for (i = 0; i < *num; i++)
+        {
+            if (! fread(result+i, sizeof(Account), 1, fp))
+            {
+                perror("Problem reading from file");
+                fclose(fp);
+
+                return NULL;
+            }
+        }
+
+        fclose(fp);
+
+        return result;
+    }
+
+    perror("File could not be opened.");
+
+    return NULL;
 }
--- account.h
+++ account.h
@@ -1,6 +1,14 @@
 typedef struct{
+  char operationSymbol;
+  double amount;
+
+}operation;
+
+typedef struct{
   long int id;
   char holderName[50];
   char holderEmail[70];
   double balance;
+  int numberOfOperations;
+ operation operations [];
 }Account;
